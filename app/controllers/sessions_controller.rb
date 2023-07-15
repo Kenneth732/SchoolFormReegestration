64c683a55d99a32c2984843d82f3fb993ad2aa82
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-15 15:07:16 +0300

    create method for sessin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
 class SessionsController < ApplicationController
 
-
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+          session[:user_id] = user.id
+          render json: user, status: :created # Fix syntax error here
+        else
+          render json: { error: "Invalid username or password" }, status: :unauthorized
+        end
+      end
 end
