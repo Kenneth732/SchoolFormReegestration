44112af2c7ab846caa37564e6c539ae19a1ece12
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-15 15:07:48 +0300

    set private method for destry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,4 +9,9 @@ class SessionsController < ApplicationController
           render json: { error: "Invalid username or password" }, status: :unauthorized
         end
       end
+    
+      def destroy
+        session[:user_id] = nil
+        head :no_content
+      end
 end
