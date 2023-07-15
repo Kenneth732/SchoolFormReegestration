33055056120df0fc6052dc87c677a405432dd6d8
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-15 15:05:49 +0300

    user_contr... create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,4 @@ class UsersController < ApplicationController
         end
       end
     
-      private
-    
-      def user_params
-        params.permit(:profile_url, :username, :password, :password_confirmation)
-      end
 end
