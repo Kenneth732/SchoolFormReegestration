bd6f343468b28893bbc5a9dd2a47b7b6a8763dac
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-15 15:24:14 +0300

    route for authe..

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show, :create, :update, :destroy]
+  post "/login", to: "sessions#create"
+  get "/me", to: "users#show"
+  delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
